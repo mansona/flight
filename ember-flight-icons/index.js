c433400e3efd096de91e9352218ad17db8fc9374
Amy Lam <amy.r.lam@gmail.com>
2021-08-24 15:07:08 -0700

    just testing ember auto import
    Co-authored-by: Michael Lange <mlange.cs@gmail.com>

--- ember-flight-icons/index.js
+++ ember-flight-icons/index.js
@@ -2,4 +2,27 @@
 
 module.exports = {
   name: require('./package').name,
+
+  // if your addon uses dynamic import
+  options: {
+    babel: {
+      plugins: [require.resolve('ember-auto-import/babel-plugin')],
+    },
+  },
+
+  autoImport: {
+    webpack: {
+      // tbd
+      module: {
+        rules: [
+          {
+            test: '/\.svg/',
+            type: 'asset/resource'
+            type: 'asset/inline'
+            type: 'asset/source'
+          }
+        ]
+      }
+    }
+  }
 };
